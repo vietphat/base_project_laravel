71d84147ea2daee33e7986760d219934f25e4ed8
lnam6507 <lnam6507@gmail.com>
2023-02-07 20:16:34 +0700

    xóa trường mật khẩu trong update

--- resources/views/admin/user/edit.blade.php
+++ resources/views/admin/user/edit.blade.php
@@ -4,15 +4,11 @@
         @csrf
         <div class="row">
             <div class="col-lg-6">
+                <x-input value="{{$user->email}}" type="email" label="Email" name="email" />
                 <x-input value="{{$user->fullname}}" label="Full Name" name="fullname" />
                 <x-input value="{{$user->phone_number_1}}" type="phone" label="Phone" name="phone_number_1" />
                 <x-input value="{{$user->address}}" type="address" label="Address" name="address" />
                 <button class="btn btn-sm shadow btn-success">Update</button>
-            </div>
-            <div class="col-lg-6">
-                <x-input value="{{$user->email}}" type="email" label="Email" name="email" />
-                <x-input type="password" label="Password" name="password" />
-                <x-input type="password" label="Confirm Password" name="confirm_password" />
             </div>
         </div>
     </form>
